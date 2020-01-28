1fe44fefa815fa6dae3396f41a5397ba44a3a730
Trent Smith <smithtrent00@gmail.com>
2020-01-28 04:09:03 +0000

    working on adding the endpoints in the function.

--- index.js
+++ index.js
@@ -1,39 +1,58 @@
 const express = require('express');
 
 const app = express();
-
+db =[];
 app.get('/', (req, res) => {
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database(':memory:');
  
 db.serialize(function() {
-  db.run("CREATE TABLE lorem (info TEXT)");
- 
-  var stmt = db.prepare("INSERT INTO lorem VALUES (?)");
-  for (var i = 0; i < 10; i++) {
-      stmt.run("Ipsum " + i);
-  }
+  db.run("CREATE TABLE updb (u TEXT, p TEXT)");
+   db.run("CREATE TABLE relationvalues (u TEXT, v1 INT, v2,INT, v3 INT)");
+
+  var stmt = db.prepare("INSERT INTO updb (u,p) VALUES (?,?)");
+
+      stmt.run("u","p");
   stmt.finalize();
  
-  db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
-     console.log(row.id + ": " + row.info);
-  });
+  
 });
  
-db.close();
+//db.close();
+this.db =db;
   res.send('Hello test')
 
 });
 
-app.get('/test', (req, res) => {
+app.get('/getAll', (req, res) => {
 
 var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database(':memory:');
+//var db = new sqlite3.Database(':memory:');
+ db.serialize(function() {
+ db.each("SELECT u,p FROM updb", function(err, row) {
+     console.log(row.p + ": " + row.u);
+  });
+    res.send('Hello test')
+
+});
+
+});
+
+app.get('/insert', (req, res) => {
+
+var sqlite3 = require('sqlite3').verbose();
+//var db = new sqlite3.Database(':memory:');
+db.serialize(function() { 
+  var stmt = db.prepare("INSERT INTO updb (u,p) VALUES (?,?)");
+
+      stmt.run("u1","p1");
+  stmt.finalize();
+     res.send('Hello test')
+
+  
+});
  
 
-   db.each("SELECT rowid AS id, info FROM lorem", function(err, row) {
-     console.log(row.id + ": " + row.info);
-  });
 });
 
 app.listen(3000, () => {
