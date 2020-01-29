eb67c2b25f592bcf1238223dde3c07716ed9583e
Trent Smith <smithtrent00@gmail.com>
2020-01-29 03:17:21 +0000

    adding select user and password and token

--- index.js
+++ index.js
@@ -8,15 +8,60 @@ var db = new sqlite3.Database(':memory:');
  
 db.serialize(function() {
   db.run("CREATE TABLE updb (u TEXT, p TEXT)");
-   db.run("CREATE TABLE relationvalues (u TEXT, v1 INT, v2,INT, v3 INT)");
+  db.run("CREATE TABLE tokdb (u TEXT, token TEXT)");
+  db.run("CREATE TABLE relationvalues (u TEXT, v1 INT, v2,INT, v3 INT)");
 
   var stmt = db.prepare("INSERT INTO updb (u,p) VALUES (?,?)");
       stmt.run("u","p");
+      stmt.run("u1","p1");
   var stmt = db.prepare("INSERT INTO relationvalues (u,v1,v2,v3) VALUES (?,?,?,?)");
       stmt.run("u",1,1,1);
   stmt.finalize();
  
   
+});
+app.get('/getpass/:u/:p', (req, res) => {
+
+    res.send('Hello test');
+    var sqlite3 = require('sqlite3').verbose();
+//var db = new sqlite3.Database(':memory:');
+ db.serialize(function() {
+
+    db.each("SELECT * FROM updb where u = "+req.params.u+" and p = "+req.params.p, function(err, row) {
+     if(req.params.u==row.u&&req.params.p==row.p)
+     {
+        console.log(row.u + ": " + row.p);
+     }
+  });
+});
+
+});
+app.get('/getuser/:u/:token', (req, res) => {
+
+    res.send('Hello test'+req.params.id);
+    var sqlite3 = require('sqlite3').verbose();
+//var db = new sqlite3.Database(':memory:');
+ db.serialize(function() {
+
+    db.each("SELECT * FROM tokdb where u = "+u+" and token = "+token, function(err, row) {
+     console.log(row.u + ": " + row.token);
+  });
+});
+
+});
+
+app.get('/getuser/:u/:token', (req, res) => {
+
+    res.send('Hello test'+req.params.id);
+    var sqlite3 = require('sqlite3').verbose();
+//var db = new sqlite3.Database(':memory:');
+ db.serialize(function() {
+
+    db.each("SELECT * FROM tokdb where u = "+u+" and token = "+token, function(err, row) {
+     console.log(row.u + ": " + row.token);
+  });
+});
+
 });
  
 //db.close();
@@ -53,6 +98,8 @@ var sqlite3 = require('sqlite3').verbose();
 });
 
 });
+
+
 
 app.get('/insert', (req, res) => {
 
