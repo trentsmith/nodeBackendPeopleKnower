42a51009b887cbcca52a0bc01ecca74d9310246e
Trent Smith <smithtrent00@gmail.com>
2020-01-29 03:29:54 +0000

    wrong branch

--- index.js
+++ index.js
@@ -1,39 +1,116 @@
 const express = require('express');
 
 const app = express();
-
+db =[];
 app.get('/', (req, res) => {
-const sqlite3 = require('sqlite3').verbose();
+var sqlite3 = require('sqlite3').verbose();
+var db = new sqlite3.Database(':memory:');
  
-// open the database
-let db = new sqlite3.Database('./db/chinook.db', sqlite3.OPEN_READWRITE, (err) => {
-  if (err) {
-    console.error(err.message);
-  }
-  console.log('Connected to the chinook database.');
+db.serialize(function() {
+  db.run("CREATE TABLE updb (u TEXT, p TEXT)");
+  db.run("CREATE TABLE tokdb (u TEXT, token TEXT)");
+  db.run("CREATE TABLE relationvalues (u TEXT, v1 INT, v2,INT, v3 INT)");
+
+  var stmt = db.prepare("INSERT INTO updb (u,p) VALUES (?,?)");
+      stmt.run("u","p");
+      stmt.run("u1","p1");
+  var stmt = db.prepare("INSERT INTO relationvalues (u,v1,v2,v3) VALUES (?,?,?,?)");
+      stmt.run("u",1,1,1);
+  stmt.finalize();
+ 
+  
 });
- 
-db.serialize(() => {
-  db.each(`create table updb(u varchar(20), p varchar(20));
-`, (err, row) => {
-    if (err) {
-      console.error(err.message);
-    }
-    console.log(row.id + "\t" + row.name);
+app.get('/getpass/:u/:p', (req, res) => {
+
+    res.send('Hello test');
+    var sqlite3 = require('sqlite3').verbose();
+//var db = new sqlite3.Database(':memory:');
+ db.serialize(function() {
+
+    db.each("SELECT * FROM updb where u = "+req.params.u+" and p = "+req.params.p, function(err, row) {
+     if(req.params.u==row.u&&req.params.p==row.p)
+     {
+        console.log(row.u + ": " + row.p);
+     }
   });
-  res.send('Hello test')
-});
- 
-db.close((err) => {
-  if (err) {
-    console.error(err.message);
-  }
-  console.log('Close the database connection.');
-});
 });
 
-app.get('/test', (req, res) => {
+});
+app.get('/getuser/:u/:token', (req, res) => {
+
+    res.send('Hello test'+req.params.id);
+    var sqlite3 = require('sqlite3').verbose();
+//var db = new sqlite3.Database(':memory:');
+ db.serialize(function() {
+
+    db.each("SELECT * FROM tokdb where u = "+u+" and token = "+token, function(err, row) {
+     console.log(row.u + ": " + row.token);
+  });
+});
+
+ db.serialize(function() {
+
+    db.each("SELECT * FROM tokdb where u = "+req.params.u+" and p = "+req.params.token, function(err, row) {
+     if(req.params.u==row.u&&req.params.token==row.token)
+     {
+        console.log(row.u + ": " + row.token);
+     }
+  });
+});
+
+});
+//db.close();
+this.db =db;
   res.send('Hello test')
+
+});
+
+app.get('/getAllrelations', (req, res) => {
+
+var sqlite3 = require('sqlite3').verbose();
+//var db = new sqlite3.Database(':memory:');
+ db.serialize(function() {
+ db.each("SELECT u,v1,v2,v3 FROM relationvalues", function(err, row) {
+     console.log(row.u + ": " + row.v1.toString());
+  });
+    res.send('Hello test')
+
+});
+
+});
+
+
+app.get('/getAllusers', (req, res) => {
+
+var sqlite3 = require('sqlite3').verbose();
+//var db = new sqlite3.Database(':memory:');
+ db.serialize(function() {
+ db.each("SELECT u,p FROM updb", function(err, row) {
+     console.log(row.p + ": " + row.u);
+  });
+    res.send('Hello test')
+
+});
+
+});
+
+
+
+app.get('/insert', (req, res) => {
+
+var sqlite3 = require('sqlite3').verbose();
+//var db = new sqlite3.Database(':memory:');
+db.serialize(function() { 
+  var stmt = db.prepare("INSERT INTO updb (u,p) VALUES (?,?)");
+
+      stmt.run("u1","p1");
+  stmt.finalize();
+     res.send('Hello test')
+
+  
+});
+ 
+
 });
 
 app.listen(3000, () => {
