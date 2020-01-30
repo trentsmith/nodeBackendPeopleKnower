f6c60d4e67a3c54e92b6dbc7710c79d5847ddca0
Trent Smith <smithtrent00@gmail.com>
2020-01-30 03:38:44 +0000

    got the insert into missing '' in code

--- index.js
+++ index.js
@@ -1,148 +1,22 @@
 const express = require('express');
 const alasql = require('alasql');
- alasql("CREATE TABLE test (language INT, hello STRING)");
+ //alasql("CREATE TABLE test (language INT, hello STRING)");
+   alasql("CREATE TABLE updb (u STRING, p STRING)");
+
+  alasql("CREATE TABLE tokdb (u STRING, token STRING)");
+ alasql("CREATE TABLE relationvalues (u STRING, v1 INT, v2 INT, v3 INT)");
 //notice look into using cors and disabling it for the time being.
 //https://expressjs.com/en/resources/middleware/cors.html
 const app = express();
 db =[];
 app.get('/', (req, res) => {
-var sqlite3 = require('sqlite3').verbose();
-var db = new sqlite3.Database(':memory:');
- 
-db.serialize(function() {
-try
-{
-  alasql("CREATE TABLE updb (u TEXT, p TEXT)");
-  alasql("CREATE TABLE tokdb (u TEXT, token TEXT)");
-  alasql("CREATE TABLE relationvalues (u TEXT, v1 INT, v2 INT, v3 INT)");
-}
-catch(err)
-{
 
-}
-  alasql("INSERT INTO updb (u,p) VALUES (?,?)");
-  alasql("INSERT INTO relationvalues (u,v1,v2,v3) VALUES (u,1,2,3)");
- 
+ // alasql("INSERT INTO updb VALUES (u,p)");
+ // alasql("INSERT INTO relationvalues VALUES (u,1,2,3)");
+ alasql("INSERT INTO updb VALUES ('u','p')");
+ res.send("test")
   
 });
-app.get('/getpass/:u/:p', (req, res) => {
-
-    res.send('Hello test');
-    var sqlite3 = require('sqlite3').verbose();
-//var db = new sqlite3.Database(':memory:');
- db.serialize(function() {
-
-    db.each("SELECT * FROM updb where u = "+req.params.u+" and p = "+req.params.p, function(err, row) {
-     if(req.params.u==row.u&&req.params.p==row.p)
-     {
-        console.log(row.u + ": " + row.p);
-     }
-  });
-});
-
-});
-app.get('/getuser/:u/:token', (req, res) => {
-
-    res.send('Hello test'+req.params.id);
-    var sqlite3 = require('sqlite3').verbose();
-//var db = new sqlite3.Database(':memory:');
- db.serialize(function() {
-
-    db.each("SELECT * FROM tokdb where u = "+req.params.u+" and p = "+req.params.token, function(err, row) {
-     if(req.params.u==row.u&&req.params.token==row.token)
-     {
-        console.log(row.u + ": " + row.token);
-     }
-  });
-});
-
-});
-//db.close();
-this.db =db;
-  res.send('Hello test')
-
-});
-
-app.get('/getAllrelations', (req, res) => {
-
-var sqlite3 = require('sqlite3').verbose();
-//var db = new sqlite3.Database(':memory:');
- db.serialize(function() {
- db.each("SELECT u,v1,v2,v3 FROM relationvalues", function(err, row) {
-     console.log(row.u + ": " + row.v1.toString());
-  });
-    res.send('Hello test')
-
-});
-
-});
-
-
-app.get('/getAllusers', (req, res) => {
-
-var sqlite3 = require('sqlite3').verbose();
-//var db = new sqlite3.Database(':memory:');
- db.serialize(function() {
- db.each("SELECT u,p FROM updb", function(err, row) {
-     console.log(row.p + ": " + row.u);
-  });
-    res.send('Hello test')
-
-});
-
-});
-
-app.get('/insertuser/:u/:p', (req, res) => {
-
-var sqlite3 = require('sqlite3').verbose();
-//var db = new sqlite3.Database(':memory:');
-db.serialize(function() { 
-  var stmt = db.prepare("INSERT INTO updb (u,p) VALUES (?,?)");
-
-      stmt.run(req.params.u,req.params.p);
-  stmt.finalize();
-     res.send('Hello test')
-
-  
-});
- 
-
-});
-
-app.get('/insertuser/:u/:p', (req, res) => {
-
-var sqlite3 = require('sqlite3').verbose();
-//var db = new sqlite3.Database(':memory:');
-db.serialize(function() { 
-  var stmt = db.prepare("INSERT INTO updb (u,p) VALUES (?,?)");
-
-      stmt.run(req.params.u,req.params.p);
-  stmt.finalize();
-     res.send('Hello test')
-
-  
-});
- 
-
-});
-
-app.get('/insertrelation/:u/:v1/:v2/:v3', (req, res) => {
-
-var sqlite3 = require('sqlite3').verbose();
-//var db = new sqlite3.Database(':memory:');
-db.serialize(function() { 
-  var stmt = db.prepare("INSERT INTO updb (u,v1,v2,v3) VALUES (?,?,?,?)");
-
-      stmt.run(req.params.u,req.params.v1,req.params.v2,req.params.v3);
-  stmt.finalize();
-     res.send('Hello test')
-
-  
-});
- 
-
-});
-
 app.listen(3000, () => {
   console.log('server started');
 });
