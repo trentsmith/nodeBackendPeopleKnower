569b280b9415cd5e8aca8ee383543905731e386a
Trent Smith <smithtrent00@gmail.com>
2020-02-01 01:50:34 +0000

    adding more endpoints

--- index.js
+++ index.js
@@ -1,36 +1,25 @@
 const express = require('express');
 const alasql = require('alasql');
- //alasql("CREATE TABLE test (language INT, hello STRING)");
-   alasql("CREATE TABLE updb (u STRING, p STRING)");
+alasql("CREATE TABLE updb (u STRING, p STRING)");
+alasql("CREATE TABLE tokdb (u STRING, token STRING)");
+alasql("CREATE TABLE relationvalues (u STRING, n STRING, v1 INT, v2 INT, v3 INT)");
 
-  alasql("CREATE TABLE tokdb (u STRING, token STRING)");
- alasql("CREATE TABLE relationvalues (u STRING, v1 INT, v2 INT, v3 INT)");
-//notice look into using cors and disabling it for the time being.
 //https://expressjs.com/en/resources/middleware/cors.html
 const app = express();
 db =[];
 app.get('/', (req, res) => {
 
- // alasql("INSERT INTO updb VALUES (u,p)");
- // alasql("INSERT INTO relationvalues VALUES (u,1,2,3)");
  res.send("test")
   
 });
 app.get('/getAllusers', (req, res) => {
 
- // alasql("INSERT INTO updb VALUES (u,p)");
- // alasql("INSERT INTO relationvalues VALUES (u,1,2,3)");
 a= alasql("select * from updb");
- res.send(a[0].u)
+ res.send(a)
   
 });
 app.get('/user/:u/:p', (req, res) => {
-
- // alasql("INSERT INTO updb VALUES (u,p)");
- // alasql("INSERT INTO relationvalues VALUES (u,1,2,3)");
- //alasql("select * ");
  users= alasql("select * from updb");
- //where u = '"+req.params.u+"' &&p='"+req.params.p+"'");
 for( i = 0; i<users.length;i++)
 {
   if(users[i].u == req.params.u&&users[i].p==req.params.p)
@@ -45,10 +34,68 @@ for( i = 0; i<users.length;i++)
 
 app.get('/insert/user/:u/:p', (req, res) => {
 
- // alasql("INSERT INTO updb VALUES (u,p)");
- // alasql("INSERT INTO relationvalues VALUES (u,1,2,3)");
- //alasql("select * ");
  alasql("INSERT INTO updb VALUES ('"+req.params.u+"','"+req.params.p+"')");
+res.send("success")
+
+  
+});
+/////////////////
+app.get('/getAlltokens', (req, res) => {
+
+a= alasql("select * from tokdb");
+ res.send(a)
+  
+});
+app.get('/token/:u/:token', (req, res) => {
+ users= alasql("select * from tokdb");
+for( i = 0; i<users.length;i++)
+{
+  if(users[i].u == req.params.u&&users[i].token==req.params.token)
+  {
+    res.send(users[i])  
+    break;
+  }
+}
+
+ res.send("Does not Exist"); 
+});
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+app.get('/insert/token/:u', (req, res) => {
+
+ alasql("INSERT INTO tokdb VALUES ('"+req.params.u+"','"+getRandomInt(1000)+"')");
+res.send("success")
+
+  
+});
+
+///////////////
+app.get('/getAllrelations', (req, res) => {
+
+a= alasql("select * from relationvalues");
+ res.send(a)
+  
+});
+app.get('/relations/:u', (req, res) => {
+ users= alasql("select * from relationvalues");
+ r = [];
+for( i = 0; i<users.length;i++)
+{
+  if( users[i].u == req.params.u )
+  {
+    r.append(users[i]);  
+  }
+}
+
+ res.send(r); 
+});
+function getRandomInt(max) {
+  return Math.floor(Math.random() * Math.floor(max));
+}
+app.get('/insert/r/:u/:n/:token/:v1/:v2/:v3', (req, res) => {
+
+ alasql("INSERT INTO relationvalues VALUES ('"+req.params.u+"','"+req.params.n+"')");
 res.send("success")
 
   
