659c849116049d1472f9cb6954b51156bef683de
Trent Smith <smithtrent00@gmail.com>
2020-04-09 03:19:34 +0000

    adding update for the relationship

--- index.js
+++ index.js
@@ -7,27 +7,39 @@ alasql("CREATE TABLE relationvalues (u STRING, n STRING, v1 INT, v2 INT, v3 INT)
 //https://expressjs.com/en/resources/middleware/cors.html
 const app = express();
 db =[];
+const axios = require('axios')
+axios.post('/todos', {
+  todo: 'Buy the milk'
+})
+.then((res) => {
+  console.log(`statusCode: ${res.statusCode}`)
+  console.log(res)
+})
+.catch((error) => {
+  console.error(error)
+})
 app.get('/', (req, res) => {
-
+ alasql("INSERT INTO updb VALUES ('u','p')");
  res.send("test")
   
 });
 app.get('/getAllusers', (req, res) => {
 
 a= alasql("select * from updb");
+
  res.send(a)
   
 });
 app.get('/user/:u/:p', (req, res) => {
- users= alasql("select * from updb");
-for( i = 0; i<users.length;i++)
+ users= alasql("select * from updb where u = '"+req.params.u+"' AND p ='"+req.params.p+"'");
+/*for( i = 0; i<users.length;i++)
 {
   if(users[i].u == req.params.u&&users[i].p==req.params.p)
   {
     res.send(users[i])  
     break;
   }
-}
+}*/
 
  res.send(users); 
 });
@@ -47,8 +59,8 @@ a= alasql("select * from tokdb");
   
 });
 app.get('/token/:u/:token', (req, res) => {
- users= alasql("select * from tokdb");
-for( i = 0; i<users.length;i++)
+ users= alasql("select * from updb where u = '"+req.params.u+"' AND p ='"+req.params.token+"'");
+/*for( i = 0; i<users.length;i++)
 {
   if(users[i].u == req.params.u&&users[i].token==req.params.token)
   {
@@ -56,7 +68,7 @@ for( i = 0; i<users.length;i++)
     break;
   }
 }
-
+*/AnimationEffect
  res.send("Does not Exist"); 
 });
 function getRandomInt(max) {
@@ -78,7 +90,7 @@ a= alasql("select * from relationvalues");
   
 });
 app.get('/relations/:u', (req, res) => {
- users= alasql("select * from relationvalues");
+ users= alasql("select * from relationvalues where u = '"+requ.params.u+"'");
  r = [];
 for( i = 0; i<users.length;i++)
 {
@@ -90,6 +102,20 @@ for( i = 0; i<users.length;i++)
 
  res.send(r); 
 });
+
+app.get('/getAllrelations', (req, res) => {
+
+a= alasql("select * from relationvalues");
+ res.send(a)
+  
+});
+app.get('/update/relations/:u/:n/:v1/:v2/:v3', (req, res) => {
+ users= alasql("update relationvalues SET v1 = '"+req.params.v1+",v2="+req.params.v2+"v3="+req.params.v3+" where u = '"+requ.params.u+"' AND n = '"+req.params.n+"'");
+ r = [];
+
+ res.send(r); 
+});
+
 function getRandomInt(max) {
   return Math.floor(Math.random() * Math.floor(max));
 }
